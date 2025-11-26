173d1c43a8230b39bc75fce24ba2d22e2e3d1237
makx <makx.work@gmail.com>
2025-11-26 20:37:08 +0200

    fix sockets ext

--- php/client.php
+++ php/client.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Swoole\Coroutine;
 use Swoole\Coroutine\Client;
 
--- php/server.php
+++ php/server.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Swoole\Server;
 
 const STEP = 4;
